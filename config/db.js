ecf9a36154f14caf492fbedf38b1d24cf7d21dca
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-24 00:55:10 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+// var connectionObj = mongoose.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'root',
+//     database:'mongo_test'
+// })
+
+// console.log(process.env.database);
 mongoose.connect('mongodb://localhost:27017/mongo_test')
 .then(conn=>{
     console.log('MongoDB connected')
